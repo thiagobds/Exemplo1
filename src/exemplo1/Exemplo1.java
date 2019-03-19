95a8c8057e63df1dee5f0459349c4d17df1836c0
Thiago Augusto Souza Santos <thiagosantos19972010@hotmail,com>
2019-03-19 20:10:48 -0300

    segundo commit

--- src/exemplo1/Exemplo1.java
+++ src/exemplo1/Exemplo1.java
@@ -3,7 +3,11 @@ package exemplo1;
 public class Exemplo1 {
 
     public static void main(String[] args) {
-       
+        
+        int a=100;
+        
+        System.out.println(a);
+             
     }
     
 }
